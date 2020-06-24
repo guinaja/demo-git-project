b466d7a7ddfbda53a9a29d39125a0c53d34c3425
PRAYOON SUNTORNKITI (CDGS-ADD) <prayoon.su@cdg.co.th>
2020-06-24 15:32:53 +0700

    add comment and checge output program

--- ExampleCode.java
+++ ExampleCode.java
@@ -1,9 +1,16 @@
-
+/**
+* The HelloWorld program implements an application that
+* simply displays "Hello World!" to the standard output.
+*
+* @author  prayoon suntornkiti
+* @version 1.0
+* @since   2020-24-06 
+*/
 public class ExampleCode {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+        System.out.println("Hello World");
     }
 
 }
